4270b1763f016f9c2bb8ef662532c2e786ca0c17
Killa <killa@ubuntu.(none)>
2014-01-06 17:48:13 +0100

    initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gruzinow::Application.config.secret_key_base = 'ea14b4804985276502007b471bc462f00bea1482d7d1a174798db7a7f446317dff2e99d1d46a371f2b07bf4137e9982bb6648c4d9dd9e39174eb626c1be869b1'
